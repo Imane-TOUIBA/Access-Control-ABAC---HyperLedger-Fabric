18284a391a276a5056c095900a0a6ffa36200f3d
Imane-TOUIBA <touibaimane2003@gmail.com>
2026-07-22 10:05:09 +0000

    commiit

---
+++ test-network-backup/setOrgEnv.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+
+
+# default to using Org1
+ORG=${1:-Org1}
+
+# Exit on first error, print all commands.
+set -e
+set -o pipefail
+
+# Where am I?
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+ORDERER_CA=${DIR}/test-network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+PEER0_ORG1_CA=${DIR}/test-network/organizations/peerOrganizations/cgn.example.com/tlsca/tlsca.cgn.example.com-cert.pem
+PEER0_ORG2_CA=${DIR}/test-network/organizations/peerOrganizations/ib.example.com/tlsca/tlsca.ib.example.com-cert.pem
+PEER0_ORG3_CA=${DIR}/test-network/organizations/peerOrganizations/hu.example.com/tlsca/tlsca.hu.example.com-cert.pem
+
+
+if [[ ${ORG,,} == "cgn" || ${ORG,,} == "digibank" ]]; then
+
+   CORE_PEER_LOCALMSPID=CGNMSP
+   CORE_PEER_MSPCONFIGPATH=${DIR}/test-network/organizations/peerOrganizations/cgn.example.com/users/Admin@cgn.example.com/msp
+   CORE_PEER_ADDRESS=localhost:7051
+   CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/test-network/organizations/peerOrganizations/cgn.example.com/tlsca/tlsca.cgn.example.com-cert.pem
+
+elif [[ ${ORG,,} == "ib" || ${ORG,,} == "magnetocorp" ]]; then
+
+   CORE_PEER_LOCALMSPID=IBMSP
+   CORE_PEER_MSPCONFIGPATH=${DIR}/test-network/organizations/peerOrganizations/ib.example.com/users/Admin@ib.example.com/msp
+   CORE_PEER_ADDRESS=localhost:9051
+   CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/test-network/organizations/peerOrganizations/ib.example.com/tlsca/tlsca.ib.example.com-cert.pem
+
+else
+   echo "Unknown \"$ORG\", please choose Org1/Digibank or Org2/Magnetocorp"
+   echo "For example to get the environment variables to set upa Org2 shell environment run:  ./setOrgEnv.sh Org2"
+   echo
+   echo "This can be automated to set them as well with:"
+   echo
+   echo 'export $(./setOrgEnv.sh Org2 | xargs)'
+   exit 1
+fi
+
+# output the variables that need to be set
+echo "CORE_PEER_TLS_ENABLED=true"
+echo "ORDERER_CA=${ORDERER_CA}"
+echo "PEER0_ORG1_CA=${PEER0_ORG1_CA}"
+echo "PEER0_ORG2_CA=${PEER0_ORG2_CA}"
+echo "PEER0_ORG3_CA=${PEER0_ORG3_CA}"
+
+echo "CORE_PEER_MSPCONFIGPATH=${CORE_PEER_MSPCONFIGPATH}"
+echo "CORE_PEER_ADDRESS=${CORE_PEER_ADDRESS}"
+echo "CORE_PEER_TLS_ROOTCERT_FILE=${CORE_PEER_TLS_ROOTCERT_FILE}"
+
+echo "CORE_PEER_LOCALMSPID=${CORE_PEER_LOCALMSPID}"
